9a3d486f88b96b83c3e1faab495462a77949fdf1
Vincent Kyi <vincekyi@gmail.com>
2015-05-11 23:14:46 -0700

    got rid of username request

--- client.c
+++ client.c
@@ -3,20 +3,15 @@
 
 int sockfd;
 
-void signal_handler(int signum){
-    printf("Caught signal %d\n", signum);
-    close(sockfd);
-    exit(signum);
-
-}
+void signal_handler(int);
 
 int main(int argc, char *argv[])
 {
-    int portno, n;
+    int  portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[BUFFER_SIZE];
+    char buffer[256];
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -36,40 +31,15 @@ int main(int argc, char *argv[])
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) //connect to server
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-
-    struct timeval t;    
-    t.tv_sec = 0;
-    t.tv_usec = 0;
-    setsockopt(
-        sockfd,     // Socket descriptor
-        SOL_SOCKET, // To manipulate options at the sockets API level
-        SO_RCVTIMEO,// Specify the receiving or sending timeouts 
-        (char *)&t, // option values
-        sizeof(t) 
-    );   
 
     t_bool online = true;
 
     signal(SIGINT, signal_handler);
-    // create username
-    printf("Enter your username: ");
-    bzero(buffer,BUFFER_SIZE);
-    fgets(buffer,BUFFER_SIZE-1,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-
-    bzero(buffer,BUFFER_SIZE);
-    n = read(sockfd,buffer,BUFFER_SIZE-1);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    printf("Conversation:***\n");
 
 
-
+    while(online){
         printf("Please enter the message: ");
         bzero(buffer,BUFFER_SIZE);
         fgets(buffer,BUFFER_SIZE-1,stdin);
@@ -92,10 +62,32 @@ int main(int argc, char *argv[])
             error("Server is down");
             online = false;
          }
-         printf("%d, %d", (int)buffer[0], n);
         printf("%s\n",buffer);
 
-    
+    }
+
     close(sockfd);
     return 0;
+}
+
+void signal_handler(int signum){
+    printf("Caught signal %d\n", signum);
+
+    char buffer[256];
+    strcpy(buffer, quit);
+    int n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    if(n==0){
+        error("Server is down");
+     }
+    printf("Exiting application");
+
+    bzero(buffer,BUFFER_SIZE);
+    n = read(sockfd,buffer,BUFFER_SIZE-1);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    close(sockfd);
+    exit(signum);
+
 }
